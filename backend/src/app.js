e3607beebf4a6dea1e01a482fa459db92718f8fd
kcarrillol2-hub <kcarrillol2@miumg.edu.gt>
2026-05-29 19:27:24 -0600

    autenticacion

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require('./routes/auth.routes');
+
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
    res.send('API funcionando');
 });
